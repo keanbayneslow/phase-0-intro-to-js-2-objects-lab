346316273d081d823f03c75cde8bef80b90ca08e
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-07-14 13:52:55 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Bruce Wayne",
+    streetAddress: "123 Wayne Street, Arkham NY 10004"
+};
+
+    function updateEmployeeWithKeyAndValue(employee, key, value) {
+        return {
+         ...employee,
+         [key]: value,
+         };
+    };
+
+
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+        return employee;
+      }
+      
+      function deleteFromEmployeeByKey(employee, key) {
+        let newEmployee = { ...employee };
+        delete newEmployee[key];
+        return newEmployee;
+      }
+      
+      function destructivelyDeleteFromEmployeeByKey(employee, key) {
+        delete employee[key];
+        return employee;
+      }
+
